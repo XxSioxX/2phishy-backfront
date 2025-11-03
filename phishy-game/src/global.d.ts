e3d7aa7aa7f6faea7b6a48a97a5b5a560a3c6d17
MAVPEREZ <https://github.com/MAVPEREZ/perez-IT128.git>
2025-11-03 20:41:46 +0800

    Remove phishy-game directory as it's no longer needed

--- phishy-game/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-export {}; // Ensures the file is treated as a module
-import Phaser from 'phaser';
-
-declare global {
-  interface Window {
-    sizeChanged: () => void;
-    game: Phaser.Game;
-  }
-}
